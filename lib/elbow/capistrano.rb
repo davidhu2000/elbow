720fdfd3dadf2830689eb9838539ce46464ee0bc
David Hu <davidhu314@gmail.com>
2018-01-02 13:42:44 -0800

    add rescue statement to rescue connection timeout

--- lib/elbow/capistrano.rb
+++ lib/elbow/capistrano.rb
@@ -13,7 +13,6 @@ def elastic_load_balancer(load_balancer_or_dns_name, *args)
   description = Aws::ElasticLoadBalancing::Client.new.describe_load_balancers
   load_balancer = description.load_balancer_descriptions.detect { |elb| elb.load_balancer_name == load_balancer_or_dns_name || elb.dns_name == load_balancer_or_dns_name }
   p '>>>>> Load Balancer'
-  p load_balancer
   p load_balancer.load_balancer_name
   p load_balancer.dns_name
 
@@ -21,18 +20,32 @@ def elastic_load_balancer(load_balancer_or_dns_name, *args)
     load_balancer.instances.map { |i| Aws::EC2::Instance.new(id: i.instance_id).data }.each do |instance|
         next if instance.state.name.to_s != 'running'
 
-        p '>>>>> Instances'
-        p instance.vpc_id
-        p instance.private_ip_address
-        p instance.public_ip_address
-        p instance.network_interfaces.first.private_ip_address
+        # p '>>>>> Instances'
+        # p instance.vpc_id
+        # p instance.private_ip_address
+        # p instance.public_ip_address
+        # p instance.network_interfaces.first.private_ip_address
 
-        hostname = if instance.vpc_id
-          instance.network_interfaces.first.private_ip_address
-        else
-          instance.public_ip_address || instance.private_ip_address
+        # hostname = if instance.vpc_id
+        #   instance.network_interfaces.first.private_ip_address
+        # else
+        #   instance.public_ip_address || instance.private_ip_address
+        # end
+
+        # if instance.private_ip_address == '172.16.3.89'
+        #   hostname = instance.network_interfaces.first.private_ip_address
+        # elsif instance.private_ip_address == '172.16.1.201'
+        #   hostname = instance.private_ip_address
+        # end
+
+        begin
+          hostname = instance.private_ip_address
+          server(hostname, *args)
+        rescue => Net::SSH::ConnectionTimeout
+          hostname = instance.network_interfaces.first.private_ip_address
+          server(hostname, *args)
         end
-        server(hostname, *args)
+
     end
   else
     raise "EC2 Load Balancer not found for #{load_balancer_or_dns_name} in region #{fetch(:aws_region)}"
